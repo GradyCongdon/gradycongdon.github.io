7d4c54ed817d503591ca12ed8485b9feed3f6c1b
Grady Congdon <gradycongdon@gmail.com>
2021-03-24 23:50:06 -0400

    dumb wiggle

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,15 @@
 console.log('welcome');
+
+function wiggle() {
+    const duration = 3000;
+    const delay = 4000;
+    document
+        .querySelectorAll('.wiggle')
+        .forEach((e, i, a) => {
+            const nodeDelay = delay + i * (duration / a.length / 4);
+            e.style.animationDuration = `${duration}ms`;
+            e.style.animationDelay = `${nodeDelay}ms`;
+        })
+}
+
+wiggle();
